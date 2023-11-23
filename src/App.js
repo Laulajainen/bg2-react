ce67fd416418a23aefea2439c51d857348ab3804
SamNosratabadi <sambadiii99@gmail.com>
2023-11-23 13:48:07 +0100

    removed app.css and fixed navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import "./App.css";
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./container/Footer/Footer";
+import MealObjects from "./components/MealObjects";
+import FoodCart from "./components/FoodCart";
+import Start from "./components/Start";
 
 import { Route, Routes } from "react-router-dom";
 
@@ -10,13 +12,16 @@ const App = () => {
     <>
       <Navbar />
       <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="Meny" element={<Menue />} />
-          <Route path="Kontakt" element={<Contact />} />
-          <Route path="Kundkorg" element={<Cart />} />
-        </Routes>
+        {
+          <Routes>
+            <Route path="" element={<Start />} />
+            <Route path="/menu" element={<MealObjects />} />
+            {/* <Route path="Kontakt" element={<Contact />} /> */}
+            {/* <Route path="Kundkorg" element={<FoodCart />} /> */}
+          </Routes>
+        }
       </div>
+      <Footer />
     </>
   );
 };
