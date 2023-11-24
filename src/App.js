e413999126948f3c8d75639bea6ceab2b75bae66
EFakLloyd <elisabeth.fak.lloyd@gmail.com>
2023-11-24 13:12:39 +0100

    kontaktsida sitter ihop m resterterande hemsida

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./container/Footer/Footer";
 import MealObjects from "./components/MealObjects";
 import FoodCart from "./components/FoodCart";
 import Start from "./components/Start";
+import Contact from "./components/Contact";
 
 import { Route, Routes } from "react-router-dom";
 
@@ -16,7 +17,7 @@ const App = () => {
           <Routes>
             <Route path="" element={<Start />} />
             <Route path="/menu" element={<MealObjects />} />
-            {/* <Route path="Kontakt" element={<Contact />} /> */}
+            <Route path="/contact" element={<Contact />} />
             {/* <Route path="Kundkorg" element={<FoodCart />} /> */}
           </Routes>
         }
