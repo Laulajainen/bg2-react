65810e0251f880fd7b349d92e6df3f1ce492821e
laulajainen <o.laulajainen@proton.me>
2023-11-27 11:12:30 +0100

    fixat lite åt sam

--- src/components/FoodCart.js
+++ src/components/FoodCart.js
@@ -8,29 +8,6 @@ export default function Foodcart({ mealData, changeMealCounter }) {
         return (
           <div key={index} className="mealBoxes">
             <h3>{meal.title}</h3>
-            <div className="allergyContainer">
-              {meal.containsNuts && (
-                <img
-                  src={"/img/nuts.jpg"}
-                  alt="Innehåller nötter"
-                  title="Innehåller nötter"
-                />
-              )}
-              {meal.containsEggs && (
-                <img
-                  src={"/img/eggs.jpg"}
-                  alt="Innehåller ägg"
-                  title="Innehåller ägg"
-                />
-              )}
-              {meal.containsGluten && (
-                <img
-                  src={"/img/gluten.jpg"}
-                  alt="Innehåller gluten"
-                  title="Innehåller gluten"
-                />
-              )}
-            </div>
             <h4 className="mealPrices">{meal.price + "kr"}</h4>
             <div className="buttonContainer">
               <MealButtons
