fd3b4a65fdde85c5d16ee12751c1d5f89a40c3c0
helgesonSE <helgezone@gmail.com>
2023-11-27 16:47:22 +0100

    Completed lifting css into styles.scss

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,17 @@
 import ContentWrapper from "./ContentWrapper";
-import BackgroundCover from "./Background";
+import BackgroundCover from "./BackgroundCover";
 
 const text = (
   <h6
     className="contact-content"
-    style={{
-      padding: "2em",
-      maxWidth: "40em",
-      backgroundColor: "rgba(255,255,255,0.5)",
-      fontSize: "21px",
-      zIndex: 1,
-      backdropFilter: "blur(4px)",
-    }}
+    // style={{
+    //   padding: "2em",
+    //   maxWidth: "40em",
+    //   backgroundColor: "rgba(255,255,255,0.5)",
+    //   fontSize: "21px",
+    //   zIndex: 1,
+    //   backdropFilter: "blur(4px)",
+    // }}
   >
     {" "}
     Kontakta oss för bokning av catering, bröllop eller svensexa{" "}
@@ -20,14 +20,14 @@ const text = (
 const text1 = (
   <h6
     className="contact-content"
-    style={{
-      padding: "2em",
-      maxWidth: "40em",
-      backgroundColor: "rgba(255,255,255,0.5)",
-      fontSize: "21px",
-      zIndex: 1,
-      backdropFilter: "blur(4px)",
-    }}
+    // style={{
+    //   padding: "2em",
+    //   maxWidth: "40em",
+    //   backgroundColor: "rgba(255,255,255,0.5)",
+    //   fontSize: "21px",
+    //   zIndex: 1,
+    //   backdropFilter: "blur(4px)",
+    // }}
   >
     {" "}
     Telefon: +46726412894{" "}
@@ -36,51 +36,37 @@ const text1 = (
 const text2 = (
   <h6
     className="contact-content"
-    style={{
-      padding: "2em",
-      maxWidth: "40em",
-      backgroundColor: "rgba(255,255,255,0.5)",
-      fontSize: "21px",
-      zIndex: 1,
-      backdropFilter: "blur(4px)",
-    }}
+    // style={{
+    //   padding: "2em",
+    //   maxWidth: "40em",
+    //   backgroundColor: "rgba(255,255,255,0.5)",
+    //   fontSize: "21px",
+    //   zIndex: 1,
+    //   backdropFilter: "blur(4px)",
+    // }}
   >
     {" "}
     Mejl: boka@bistro.net{" "}
   </h6>
 );
-const map = (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "row",
-      margin: "10vw",
-      padding: "2em",
-      backgroundColor: "rgba(255,255,255,0.5)",
-      fontSize: "21px",
-      zIndex: 1,
-      backdropFilter: "blur(4px)",
-    }}
-  >
     <h6 className="contact-content">
       {" "}
       <img src="./assets/map.JPG" style={{ width: "100%" }} />
       <div
-        style={{
-          padding: "5vh",
-          display: "flex",
-          flexDirection: "column",
-          alignContent: "center",
-          justifyContent: "center",
-          fontSize: "21px",
-        }}
+        // style={{
+        //   padding: "5vh",
+        //   display: "flex",
+        //   flexDirection: "column",
+        //   alignContent: "center",
+        //   justifyContent: "center",
+        //   fontSize: "21px",
+        // }}
       >
         <div>Tucgatan 1</div>
         <div>565 37 Linköping</div>
       </div>{" "}
     </h6>
-  </div>
-);
+;
 const text3 = (
   <h6 className="contact-content">
     {" "}
@@ -93,100 +79,99 @@ const url =
 
 function Maps() {
   return (
-    <div className="click-maps" onClick={() => window.open(url, "_blank")}>
-      <div
-        className="click-maps"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          margin: "10vw",
-          padding: "2em",
-          backgroundColor: "rgba(255,255,255,0.5)",
-          fontSize: "21px",
-          zIndex: 1,
-          backdropFilter: "blur(4px)",
-        }}
-      >
-        <h6 className="contact-content">
-          {" "}
-          <img
-            src="./assets/map.JPG"
-            onClick={() => window.open(url, "_blank")}
-            style={{ width: "100%", cursor: "pointer" }}
-          />
+    <div
+      className="click-maps"
+      // style={{
+      //   display: "flex",
+      //   flexDirection: "row",
+      //   margin: "10vw",
+      //   padding: "2em",
+      //   backgroundColor: "rgba(255,255,255,0.5)",
+      //   fontSize: "21px",
+      //   zIndex: 1,
+      //   backdropFilter: "blur(4px)",
+      // }}
+    >
+      <h6 className="contact-content">
+        {" "}
+        <img
+          src="./assets/map.JPG"
+          onClick={() => window.open(url, "_blank")}
+          // style={{ width: "100%", cursor: "pointer" }}
+        />
+        <div
+          // style={{
+          //   padding: "5vh",
+          //   display: "flex",
+          //   flexDirection: "column",
+          //   alignContent: "center",
+          //   justifyContent: "center",
+          //   fontSize: "21px",
+          // }}
+        >
           <div
-            style={{
-              padding: "5vh",
-              display: "flex",
-              flexDirection: "column",
-              alignContent: "center",
-              justifyContent: "center",
-              fontSize: "21px",
-            }}
+            onClick={() => window.open(url, "_blank")}
+            style={{ cursor: "pointer" }}
           >
-            <div
-              onClick={() => window.open(url, "_blank")}
-              style={{ cursor: "pointer" }}
-            >
-              Tucgatan 1
-            </div>
-            <div
-              onClick={() => window.open(url, "_blank")}
-              style={{ cursor: "pointer" }}
-            >
-              565 37 Linköping
-            </div>
-          </div>{" "}
-        </h6>
-      </div>
+            Tucgatan 1
+          </div>
+          <div
+            onClick={() => window.open(url, "_blank")}
+            style={{ cursor: "pointer" }}
+          >
+            565 37 Linköping
+          </div>
+        </div>{" "}
+      </h6>
     </div>
   );
 }
 
 export default function Contact() {
   return (
-    <>
+    <div id="contact">
       <ContentWrapper>
         <div
           className="contact_info"
-          style={{
-            display: "flex",
-            justifyContent: "space-evenly",
-            textAlign: "center",
-          }}
+          // style={{
+          //   display: "flex",
+          //   justifyContent: "space-evenly",
+          //   textAlign: "center",
+          // }}
         >
           <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              backgroundColor: "rgba(255,255,255,0.5)",
-              fontSize: "21px",
-              flexDirection: "column",
-              zIndex: 1,
-              backdropFilter: "blur(4px)",
-              maxWidth: "100vw",
-            }}
+            className="contact-container"
+            // style={{
+            //   display: "flex",
+            //   justifyContent: "center",
+            //   backgroundColor: "rgba(255,255,255,0.5)",
+            //   fontSize: "21px",
+            //   flexDirection: "column",
+            //   zIndex: 1,
+            //   backdropFilter: "blur(4px)",
+            //   maxWidth: "100vw",
+            // }}
           >
             <h1>Kontakt</h1>
             <div
               className="contact_one"
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                flexDirection: "column",
-                width: "40em",
-              }}
+              // style={{
+              //   display: "flex",
+              //   justifyContent: "center",
+              //   flexDirection: "column",
+              //   width: "40em",
+              // }}
             >
               <div style={{ display: "flex", justifyContent: "center" }}>
                 {text}
               </div>
               <div
-                className="contact_one"
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-around",
-                }}
+                className="contact_two"
+                // style={{
+                //   display: "flex",
+                //   flexDirection: "row",
+                //   justifyContent: "space-around",
+                // }}
               >
                 {text1}
                 {text2}
@@ -200,6 +185,6 @@ export default function Contact() {
 
         <BackgroundCover />
       </ContentWrapper>
-    </>
+    </div>
   );
 }
