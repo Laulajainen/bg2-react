35b3469559725b7a247b94fcc610fa64a55b20d5
SamNosratabadi <sambadiii99@gmail.com>
2023-11-28 15:37:45 +0100

    new in

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,15 @@
 import "./Sidebar.css";
 import Foodcart from "../FoodCart";
+import { useState } from "react";
 
 export default function Sidebar({ show, mealData, changeMealCounter }) {
+  const hasItems = mealData.some((item) => item.count > 0);
+
+  const handleConfirm = () => {
+    // Add your logic for handling confirmation here
+    console.log("Confirm button clicked!");
+  };
+
   return (
     <div className="sidebar">
       <h1>Kundkorg</h1>
@@ -10,6 +18,11 @@ export default function Sidebar({ show, mealData, changeMealCounter }) {
           <Foodcart mealData={mealData} changeMealCounter={changeMealCounter} />
         ) : null}
       </div>
+      {hasItems && (
+        <button className="confirm-button" onClick={handleConfirm}>
+          Confirm Order
+        </button>
+      )}
     </div>
   );
 }
